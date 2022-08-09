21db3ea229a4ddead4e1fbd8759662ff4afd2c8c
Vetali1985 <geelycross1985@gmail.com>
2022-08-09 18:40:31 +0300

    09.08.22

---
+++ js/01-gallery.js
@@ -0,0 +1,48 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const imageCont = document.querySelector(`.gallery`)
+const imageMarkup = createGalleryItems(galleryItems)
+
+
+imageCont.insertAdjacentHTML(`beforeend`, imageMarkup)
+imageCont.addEventListener(`click`, onImgClick)
+
+
+function createGalleryItems(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+    />
+            </a>
+        </div>
+        `
+    })
+        .join(" ");
+}
+
+function onImgClick(evt) {
+    blockStandartActio(evt)
+
+    if (evt.target.nodeName !== "IMG") {
+        return
+    }
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`)
+
+    instance.show()
+    imageCont.addEventListener(`keydown`, evt => {
+        if (evt.code === "Escape")
+            instance.close()
+    })
+}
+
+
